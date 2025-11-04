b692ac3facd2fc3242320795c924c0efb5893387
timothy adigun <adigun.timothy@gmail.com>
2025-11-04 12:42:35 +0000

    run option included

--- makeod.sh
+++ makeod.sh
@@ -1,20 +1,43 @@
 #!/usr/bin/env bash
-# author: 	omitida
+# author: 	    omitida
 # description:  making generic odin file, compile and run such.
-# date: 	4th Nov. 2025
+# date: 	    4th Nov. 2025
 #
 
 function make_generic_file() {
     filename="${1}"
     filename="${filename%.*}.odin"
-    touch "${filename}"
-    echo "package ${filename%.*}" > "${filename}"
-    echo >> "${filename}"
-    echo "import \"core:fmt\"" >> "${filename}"
-    echo >> "${filename}"
-    echo "main :: proc() {" >> "${filename}"
-    echo "  fmt.println(\"Hellope!\")" >> "${filename}"
-    echo "}" >> "${filename}"
+
+    # check if the file exist
+    if ! [[ -e "${filename}" ]]; then
+        touch "${filename}"
+        echo "package ${filename%.*}" > "${filename}"
+        echo >> "${filename}"
+        echo "import \"core:fmt\"" >> "${filename}"
+        echo >> "${filename}"
+        echo "main :: proc() {" >> "${filename}"
+        echo "  fmt.println(\"Hellope!\")" >> "${filename}"
+        echo "}" >> "${filename}"
+    else
+        echo "${filename} exist."
+        while read -p "Will you like to replace the file? [y|n]: " ans; do
+            case "${ans}" in
+                y)
+                    rm -f "${filename}"
+                    echo "${filename} deleted!."
+                    exit 0
+                    ;;
+                n)
+                    echo "${filename} NOT deleted."
+                    exit 0;
+                    ;;
+                *)
+                    echo "Invalid Input."
+                    continue
+                    ;;
+            esac
+        done
+    fi
 }
 
 function helper() {
@@ -36,9 +59,18 @@ while getopts "${opstring}" opt; do
     case "${opt}" in
         g)
             file="${OPTARG}"
+            filename="${file}"
             make_generic_file "${file}"
+            folder_name="${file%.*}"
+            mkdir "${folder_name}"
+            mv "${filename}" "${folder_name}"
+
+            # run the directory..
+            ./"${0}" -r "${folder_name}"
             ;;
         r)
+            file="${OPTARG}"
+            odin run "${file}" -vet -strict-style
             ;;
         h)
             # call helper function
